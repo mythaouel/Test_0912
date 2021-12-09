51e49a2ce34104a83fb3c2420918611da1acc04c
lnghle306uel <lelnh19411@st.uel.edu.vn>
2021-12-09 22:20:55 +0700

    Push adapter

--- app/src/main/java/com/example/adapter/ProductAdapter.java
+++ app/src/main/java/com/example/adapter/ProductAdapter.java
@@ -7,19 +7,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.model.Product;
+import com.example.test_0912.R;
 
 import java.util.List;
 
 public class ProductAdapter extends BaseAdapter {
     Activity context;
-    int custom_list_view;
+    int custom_item;
     List<Product> products;
 
-    public ProductAdapter(Activity context, int custom_list_view, List<Product> products) {
+    public ProductAdapter(Activity context, int custom_item, List<Product> products) {
         this.context = context;
-        this.custom_list_view = custom_list_view;
+        this.custom_item = custom_item;
         this.products = products;
     }
 
@@ -44,17 +47,29 @@ public class ProductAdapter extends BaseAdapter {
         if (view == null){
             holder = new ViewHolder();
             LayoutInflater inflater = (LayoutInflater) context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
-            view = inflater.inflate( custom_list_view,null );
+            view = inflater.inflate( custom_item,null );
 
+            holder.imvThumb = view.findViewById( R.id.imvThumb );
+            holder.txtName = view.findViewById( R.id.txtName );
+            holder.txtDes = view.findViewById( R.id.txtDes );
+            holder.txtRating = view.findViewById( R.id.txtRating );
+            holder.txtRatingNumber = view.findViewById( R.id.txtRatingNumber );
 
             view.setTag( holder );
         }else{
             holder = (ViewHolder) view.getTag();
         }
+        Product p = products.get( i );
+        holder.imvThumb.setImageResource(p.getPdThumb());
+        holder.txtName.setText( p.getPdName() );
+        holder.txtDes.setText( p.getPdDesciption() );
+        holder.txtRating.setText( p.getPdRate() +"" );
+        holder.txtRatingNumber.setText("(" + p.getPdCount() + ")");
         return view;
     }
 
     private static class ViewHolder{
-
+        ImageView imvThumb;
+        TextView  txtName, txtDes, txtRating, txtRatingNumber;
     }
 }
