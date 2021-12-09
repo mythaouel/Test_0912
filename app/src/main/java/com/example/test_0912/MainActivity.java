a523c518e2ae5ac384b176bdc352c5d442c3dcf5
mythaouel <thaottm19411@st.uel.edu.vn>
2021-12-09 22:14:18 +0700

    MT

--- app/src/main/java/com/example/test_0912/MainActivity.java
+++ app/src/main/java/com/example/test_0912/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.test_0912;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imvLunchBox, imvNoodle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linkViews();
+        addData();
+    }
+
+    private void addData() {
+        imvLunchBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        imvNoodle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void linkViews() {
+        imvLunchBox = findViewById(R.id.imvLunchBox);
+        imvNoodle = findViewById(R.id.imvNoodle);
     }
 }
