8cf2c117d195eb557b37a099250eb7f4d2f01d46
mythaouel <thaottm19411@st.uel.edu.vn>
2021-12-09 22:22:20 +0700

    MT_Lunch_Noodle

--- app/src/main/java/com/example/test_0912/Lunch.java
+++ app/src/main/java/com/example/test_0912/Lunch.java
@@ -2,13 +2,48 @@ package com.example.test_0912;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.example.adapter.ProductAdapter;
+import com.example.model.Product;
+
+import java.util.ArrayList;
 
 public class Lunch extends AppCompatActivity {
+
+    ImageView imgBack;
+    ListView lvProduct;
+    ProductAdapter adapter;
+    ArrayList<Product> productArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch);
+        linkViews();
+        addEvents();
+    }
+
+    private void addEvents() {
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Lunch.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void linkViews() {
+        imgBack = findViewById(R.id.imgback);
+        lvProduct = findViewById(R.id.lvProduct);
+        productArrayList = new ArrayList<>();
+        adapter = new ProductAdapter(Lunch.this,
+                R.layout.custom_item, productArrayList);
+        lvProduct.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/test_0912/Noodle.java
+++ app/src/main/java/com/example/test_0912/Noodle.java
@@ -2,13 +2,46 @@ package com.example.test_0912;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.example.adapter.ProductAdapter;
+import com.example.model.Product;
+
+import java.util.ArrayList;
 
 public class Noodle extends AppCompatActivity {
 
+    ImageView imgBack;
+    ListView lvProduct;
+    ProductAdapter adapter;
+    ArrayList<Product> productArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noodle);
+
+        linkViews();
+        addEvents();
+    }
+    private void addEvents() {
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Noodle.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void linkViews() {
+        imgBack = findViewById(R.id.imgback);
+        lvProduct = findViewById(R.id.lvProduct);
+        productArrayList = new ArrayList<>();
+        adapter = new ProductAdapter(Noodle.this, R.layout.custom_item, productArrayList);
+        lvProduct.setAdapter(adapter);
     }
 }
